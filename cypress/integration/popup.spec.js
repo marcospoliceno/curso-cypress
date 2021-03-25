0fe2c2ad2a647f4265cdd9fe37a570b81524a46b
marcos.policeno <des234@dahdes.local>
2021-03-25 16:00:52 -0300

    add readme and popup

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -11,8 +11,6 @@ describe('work whith POP UP', () => {
             expect(msg).to.be.equal('Click OK!')
         })
     })
-
-
     it('Deve verificar se o popup foi invocado', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.window().then(win => {
@@ -20,7 +18,31 @@ describe('work whith POP UP', () => {
         })
         cy.get('#buttonPopUp').click()
         cy.get('@winOpen').should('be.called')
+    })
 
+    describe.only('With Links...', () => {
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+
+        it('Check popUp url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+        })
+        it('Should access popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield').type('funciona')
+            })
+        })
+        it('Should force link on some page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+                cy.get('#tfield').type('funciona')
+        })
     })
 })
      // cy.wrap(body).find('#otherButton').click()
